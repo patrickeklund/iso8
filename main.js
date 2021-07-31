007bbb3cc973dd86ea9fb36bb9b1149d6e244c8e
Castle <castlerock88@protonmail.com>
2021-07-31 08:57:02 -0400

    added comments to explain keyvalue pairs and what loop is doing

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const isoCrystals = {
-	1:0,
-	2:1000,
-	3:4000,
-	4:15000,
-	5:50000,
+const isoCrystals = {     // Level : Ions to upgrade
+	1: 0,
+	2: 1000,
+	3: 4000,
+	4: 15000,
+	5: 50000,
 }
 
-const upgradeCrystals = {
-	1:0,
-	2:5000,
-	3:15000,
-	4:25000,
-	5:50000,
+const upgradeCrystals = {    // Level : Ions to upgrade
+	1: 0,
+	2: 5000,
+	3: 15000,
+	4: 25000,
+	5: 50000,
 }
 
 const isoLevel = document.querySelector('#iso-input').value
 const calcPrint = document.querySelector('#calcResult')
 
 document.querySelector('#btn').addEventListener('click', getIons)
-	function getIons(){
-		let total = 0	
+function getIons() {
+	let total = 0
 
-		if(isoCrystals.hasOwnProperty(isoLevel) === true && upgradeCrystals.hasOwnProperty(isoLevel) === true){
-			for(let i = isoLevel; i < 6; i++){
-				total = total + (isoCrystals[i] * 5 + upgradeCrystals[i])
-				calcPrint.innerText = "you need " + total.toLocaleString('en') + " ions for level iso8 green"
-			}			
-		}else{
-			calcPrint.innerText = "Please enter a level under 6"
+	if (isoCrystals.hasOwnProperty(isoLevel) === true && upgradeCrystals.hasOwnProperty(isoLevel) === true) {   // loop to add cost of ions per level above entered value up to level 5
+		for (let i = isoLevel; i < 6; i++) {
+			total = total + (isoCrystals[i] * 5 + upgradeCrystals[i])
+			calcPrint.innerText = "you need " + total.toLocaleString('en') + " ions for level iso8 green"
 		}
-	
+	} else {
+		calcPrint.innerText = "Please enter a level under 6"
+	}
+
 }

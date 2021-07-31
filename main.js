7c711130617a39ecc7ad31d43223f65efecafaaa
Castle <castlerock88@protonmail.com>
2021-07-31 06:57:48 -0400

    changed names to publish on github

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ document.querySelector('#btn').addEventListener('click', getIons)
 		if(isoCrystals.hasOwnProperty(isoLevel) === true && upgradeCrystals.hasOwnProperty(isoLevel) === true){
 			for(let i = isoLevel; i < 6; i++){
 				total = total + (isoCrystals[i] * 5 + upgradeCrystals[i])
-				calcPrint.innerText = "you need " + total.toLocaleString('en') + " ions"
+				calcPrint.innerText = "you need " + total.toLocaleString('en') + " ions for level 5 iso8 green"
 			}			
 		}else{
 			calcPrint.innerText = "Please enter a level under 6"

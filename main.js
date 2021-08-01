6a8b82c2c91b7411d13f95f0ce2fadac76989683
Castle <castlerock88@protonmail.com>
2021-08-01 02:58:09 -0400

    fixed caching problem with help from discord added crystal calculator function

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+
+// Ion calculator
+
 const isoCrystals = {     // Level : Ions to upgrade
+	0: 0,
 	1: 0,
 	2: 1000,
 	3: 4000,
@@ -7,33 +11,56 @@ const isoCrystals = {     // Level : Ions to upgrade
 }
 
 const upgradeCrystals = {    // Level : Ions to upgrade
+	0: 0,
 	1: 0,
 	2: 5000,
 	3: 15000,
 	4: 25000,
 	5: 50000,
 }
+// Object.keys(isoCrystals).length
 
-const isoLevel = document.querySelector('#iso-input').value
 const calcPrint = document.querySelector('#calcResult')
 
 document.querySelector('#btn').addEventListener('click', getIons)
 function getIons() {
+	const isoLevel = document.querySelector('#iso-input').value
 	let total = 0
 	if (isoCrystals.hasOwnProperty(isoLevel) === true && upgradeCrystals.hasOwnProperty(isoLevel) === true) {   // loop to add cost of ions per level above entered value up to level 5
 		for (let i = isoLevel; i < 6; i++) {
 			total = total + (isoCrystals[i] * 5 + upgradeCrystals[i])
-			let answer = "you need " + total.toLocaleString('en') + " ions for level iso8 green" 
-			calcPrint.innerText = answer
+			calcPrint.innerText = "you need " + total.toLocaleString('en') + " ions for level iso8 green" 
 		}
 	} else {
 		calcPrint.innerText = "Please enter a level under 6"
 	}
-
 }
 
+// crystal calculator
 
-document.querySelector('#btn2').addEventListener('click', clear)
-function clear(){
-	location.reload();
+
+const crystalCount = {
+	0: 0,
+	1: 1,
+	2: 3,
+	3: 9,
+	4: 27,
+	5: 81,
+}
+
+const crystalsToMax = document.querySelector('#calcResult2')
+
+
+document.querySelector('#btn2').addEventListener('click', maxCrystals)
+function maxCrystals(){
+	const crystalLevel = document.querySelector('#crystal-input').value
+	let total = 0
+	if(crystalCount.hasOwnProperty(crystalLevel) === true){
+		for (let i = crystalLevel; i < 6; i++) {
+			total = total + crystalCount[i]
+			crystalsToMax.innerText = "you need " + total + " crystals to max"
+		}
+	}else {
+		crystalsToMax.innerText = "Please enter a level under 6"
+	}
 }
